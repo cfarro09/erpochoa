ffa818fbdd75d27b2b843041206d9326ff9be848
roberto velasquez <fdcarlosd1@gmail.com>
2019-12-28 17:14:56 -0500

    listadoxyz11

--- setVenta.php
+++ setVenta.php
@@ -1,7 +1,6 @@
 <?php
 require_once('Connections/Ventas.php');
 mysql_select_db($database_Ventas, $Ventas);
-
 if (isset($_POST['json'])) {
 	$json = $_POST['json'];
 }
@@ -10,14 +9,14 @@ $queryheader = json_decode($json)->header;
 $detalleArray = json_decode($json)->detalle;
 
 if($queryheader){
-	mysql_query($queryheader, $Ventas) or die(mysql_error());
-	$lastId = mysql_query("SELECT LAST_INSERT_ID()", $Ventas) or die(mysql_error());
+	mysql_query($queryheader, $Ventas) or die(mysql_error() . "  " . $queryheader);
+	$lastId = mysql_query("SELECT LAST_INSERT_ID()", $Ventas) or die(mysql_error(). "  ". $queryheader);
 	$lastId = (int) mysql_fetch_assoc($lastId)["LAST_INSERT_ID()"];
 }
 
 foreach ($detalleArray as $querydetalle) {
 	$querydetalle = str_replace("###ID###", $lastId, $querydetalle);
-	mysql_query($querydetalle, $Ventas) or die(mysql_error());
+	mysql_query($querydetalle, $Ventas) or die(mysql_error() . "  " . $querydetalle);
 }
 
 die(json_encode(array("success" => true), 128));
