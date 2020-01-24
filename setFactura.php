2eb13de744b1bbef50a7ce7682a8f09123d370eb
Carlos Farro <fdcarlosd1@gmail.com>
2020-01-23 22:56:32 -0500

    avance

--- setFactura.php
+++ setFactura.php
@@ -61,7 +61,7 @@ if($h->codigocompras){
   $maxcodigo = (int) mysql_fetch_object($result)->maxcodigo + 1;
 
   $contamaxcodigo = $h->codigomesconta.$maxcodigo
-  $insertCabecera = "insert into registro_compras(tipomoneda, tipo_comprobante, rucproveedor, numerocomprobante, codacceso, subtotal, igv, total, estadofact, codigosuc, codigo_orden_compra, codigo_guia_sin_oc, fecha_registro, valorcambio, descuentocompras, codigoproveedor, codigomesconta) values ('$h->tipomoneda', '$h->tipo_comprobante', '$h->ruc_proveedor', '$h->numerocomprobante', $h->codacceso, $h->subtotal, $h->igv, $h->total, $h->estadofact, $h->codigosuc, $h->codigo_orden_compra, $h->codigo_guia_sin_oc, '$h->fecha_registro', $h->valorcambio, $h->descuentocompras, $h->codigoproveedor, '$contamaxcodigo')";
+  $insertCabecera = "insert into registro_compras(tipomoneda, tipo_comprobante, rucproveedor, numerocomprobante, codacceso, subtotal, igv, total, estadofact, codigosuc, codigo_orden_compra, codigo_guia_sin_oc, fecha_registro, valorcambio, descuentocompras, codigoproveedor, codigomesconta, contamesincrement) values ('$h->tipomoneda', '$h->tipo_comprobante', '$h->ruc_proveedor', '$h->numerocomprobante', $h->codacceso, $h->subtotal, $h->igv, $h->total, $h->estadofact, $h->codigosuc, $h->codigo_orden_compra, $h->codigo_guia_sin_oc, '$h->fecha_registro', $h->valorcambio, $h->descuentocompras, $h->codigoproveedor, '$contamaxcodigo', $maxcodigo)";
 
   $queryHeader = mysql_query($insertCabecera, $Ventas) or die(mysql_error());
 
