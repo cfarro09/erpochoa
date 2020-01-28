ecad9d81ea8c0d2761258ebdaf21a24e3b4634b6
Carlos Farro <carlos.farro@vcaperu.com>
2020-01-27 22:15:44 -0500

    avance

--- compras_insumos.php
+++ compras_insumos.php
@@ -34,56 +34,63 @@ $codsucursal = $_SESSION['cod_sucursal'];
 <form id="form-generate-compra">
 	<div class="row">
 		<div class="col-sm-12 text-center">
-			<button class="btn btn-success" type="submit" style="margin-top:10px;margin-bottom: 10px; font-size: 20px">COMPRAR</button>
+			<button class="btn btn-success" type="submit" style="margin-top:10px;margin-bottom: 10px; font-size: 20px">CONFIRMAR COMPRA</button>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-sm-12">
 
 			<div class="row" style="margin-top: 10px">
-				<div class="col-md-6">
-					<div class="form-group">
-						<label for="field-1" class="control-label">Proveedor</label>
-						<select id="proveedorx" class="form-control"></select>
-					</div>
-				</div>
-				<div class="col-md-6" style="display: none" id="div_aux"></div>
-				<div class="col-md-6" id="div_direccion" style="display: none">
-					<div class="form-group">
-						<label for="direccion" class="control-label">Direccion</label>
-						<input type="text" class="form-control" id="direccion">
-					</div>
-				</div>
 
-				<div class="col-md-6">
+
+				<div class="col-md-3">
 					<div class="form-group">
-						<label for="field-1" class="control-label">Documento de Referencia</label>
-						<input type="text" class="form-control" required="" id="codigoreferencial1" name="codigoreferencial1">
+						<label for="field-1" class="control-label">Tipo Compra</label>
+						<select id="tipocompra" required class="form-control">
+							<option value="mercaderia">Mercaderia</option>
+							<option value="insumos">Insumos</option>
+							<option value="servicios">Servicios</option>
+						</select>
 					</div>
 				</div>
-				<div class="col-md-6">
+				<div class="col-md-3">
 					<div class="form-group">
-						<label for="field-1" class="control-label">Documento de referencia</label>
-						<input type="text" class="form-control" id="codigoreferencia2" name="codigoreferencia2">
+						<label for="field-1" class="control-label">Comprobante</label>
+						<select required id="comprobantex" class="form-control">
+							<option value="factura">Factura</option>
+							<option value="boleta">Boleta</option>
+							<option value="notadebito">Nota Debito</option>
+							<option value="notacredito">Nota Credito</option>
+							<option value="otros">Otros</option>
+						</select>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="form-group">
+						<label for="nrocomprobante" class="control-label">N° Comprobante</label>
+						<input required type="text" class="form-control" id="nrocomprobante">
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="form-group">
+						<label for="cuentax" class="control-label">Cuenta</label>
+						<input required type="text" class="form-control" id="cuentax">
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="form-group">
+						<label for="proveedorx" class="control-label">Proveedor</label>
+						<select required id="proveedorx" class="form-control"></select>
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>
-	<div class="row" style="display: none">
-		<div class="col-sm-12 text-center">
-			<button class="btn btn-success" type="submit" id="generateCompra" style="margin-top:10px;margin-bottom: 10px; font-size: 20px">GENERAR ORDEN DE COMPRA</button>
 		</div>
 	</div>
 	<button type="button" onclick="agregarproducto()" class="btn btn-primary">Agregar Producto</button>
 	<div class="row" style="margin-top:20px">
 		<div class="col-sm-12">
 			<table class="table table-bordered" style="width: 100%">
-				<col width="30">
-				<col width="80">
-				<col width="30">
-				<col width="30">
-				<col width="30">
+
 				<thead>
 					<th style="font-weight: bold">CANTIDAD</th>
 					<th style="font-weight: bold">PRODUCTO</th>
@@ -116,8 +123,12 @@ include("Fragmentos/pie.php");
 
 <script>
 	$(function() {
-		initControls()
+		initEvents();
+		initControls();
 	});
+	const initEvents = () => {
+		getSelector("#form-generate-compra").addEventListener("submit", guardar);
+	}
 	const initControls = () => {
 		loadproveedor();
 	}
@@ -125,16 +136,16 @@ include("Fragmentos/pie.php");
 		detalleFormProducto.innerHTML += `
 			<tr class="divparent">
 				<td width="15%">
-					<input type="number" class="form-control cantidad" data-type="cantidad" value="1" oninput="calcularimporte(this)">
+					<input required type="number" class="form-control cantidad" data-type="cantidad" value="1" oninput="calcularimporte(this)">
 				</td>
 				<td width="40%">
-					<input type="text" class="form-control producto">
+					<input required type="text" class="form-control producto">
 				</td>
 				<td width="15%">
-					<input type="number" class="form-control precio" data-type="precio" value="0" oninput="calcularimporte(this)">
+					<input required type="number" class="form-control precio" data-type="precio" value="0" oninput="calcularimporte(this)">
 				</td>
 				<td width="15%">
-					<input type="text" class="form-control importe" value="0" disabled>
+					<input required type="text" class="form-control importe" value="0" disabled>
 				</td>
 				<td width="15%" class="text-center">
 					<button type="button" onclick="eliminarproducto(this)" class="btn red-thunderbird btn-sm"><i class="glyphicon glyphicon-trash"></i></button>
@@ -142,8 +153,66 @@ include("Fragmentos/pie.php");
 			</tr>
 		`;
 	}
+	const guardar = async e => {
+		e.preventDefault();
+		const data = {
+			header: {},
+			detalle: []
+		}
+		const nowx = new Date();
+		const month = nowx.getMonth() + 1;
+
+		const codigomesconta = `${nowx.getFullYear()}${month}-`;
+		const firstday = `${nowx.getFullYear()}-${nowx.getMonth() + 1}-1`;
+
+		const querymaxcode = `SELECT IFNULL(max(contamesincrement), 0) maxcodigo FROM registro_compras WHERE fecha BETWEEN '${firstday}' and LAST_DAY('${firstday}')`;
+		const result = await get_data_dynamic(querymaxcode);
+		const maxcodigo = result[0].maxcodigo;
+		const codeconcat = `${codigomesconta}${maxcodigo}`
+
+		const rucproveedor = proveedorx.options[proveedorx.selectedIndex].dataset.ruc;
+		const codproveedor = proveedorx.value;
+		const codacceso = <?= $_SESSION['kt_login_id'] ?>;
+		const sucursal = <?= $codsucursal ?>;
+		const subtotalh = subtotalheader.textContent;
+		const igvh = igvheader.textContent;
+		const totalh = totalheader.textContent;
+
+		data.header = `
+			insert into registro_compras(tipo_comprobante, rucproveedor, numerocomprobante, codacceso, subtotal, igv, total, estadofact, codigosuc, fecha_registro, codigoproveedor, codigomesconta, contamesincrement) values ('${comprobantex.value}', '${rucproveedor}', '${nrocomprobante.value}', ${codacceso}, ${subtotalh}, ${igvh}, ${totalh}, 0, ${sucursal}, NOW(), ${codproveedor}, '${codeconcat}', ${maxcodigo})
+		`;
+		getSelectorAll("#detalleFormProducto tr").forEach(x => {
+			const cantidad = x.querySelector(".cantidad").value;
+			const producto = x.querySelector(".producto").value;
+			const precio = x.querySelector(".precio").value;
+			const subtotal = precio * cantidad;
+			const total = subtotal * IGV1;
+			const igv = subtotal * IGV;
+			const query = `
+				insert into detalle_compras(codigoprod, cantidad, vcu, vci, vcf, igv, totalcompra, codigocompras, nombreproducto) values (0, ${cantidad}, ${precio}, ${subtotal}, ${subtotal}, ${igv}, ${total}, ###ID###, '${producto}')
+			`;
+			data.detalle.push(query)
+		})
+
+		var formData = new FormData();
+		formData.append("json", JSON.stringify(data))
+
+		fetch(`setVenta.php`, {
+				method: 'POST',
+				body: formData
+			})
+			.then(res => res.json())
+			.catch(error => console.error("error: ", error))
+			.then(res => {
+				if (res.success) {
+					alert("registro completo!")
+					location.reload();
+
+				}
+			});
+	}
 	const calcularimporte = e => {
-		if(e.value < 0){
+		if (e.value < 0) {
 			e.value = 0;
 			return
 		}
@@ -160,18 +229,18 @@ include("Fragmentos/pie.php");
 		getSelectorAll("#detalleFormProducto .importe").forEach(x => {
 			total += parseFloat(x.value);
 		});
-		totalheader.textContent = (total*IGV1).toFixed(2);
+		totalheader.textContent = (total * IGV1).toFixed(2);
 		subtotalheader.textContent = total.toFixed(2);
-		igvheader.textContent = (total*IGV).toFixed(2);
+		igvheader.textContent = (total * IGV).toFixed(2);
 	}
 	const eliminarproducto = e => {
 		e.closest(".divparent").remove();
 		calculartotales();
 	}
 	const loadproveedor = async () => {
-		const query = "SELECT codigoproveedor as codigoclienten, CONCAT(razonsocial, ' ', ruc) as name FROM proveedor  WHERE estado = 0 order by razonsocial";
+		const query = "SELECT ruc, codigoproveedor as codigoclienten, CONCAT(razonsocial, ' ', ruc) as name FROM proveedor  WHERE estado = 0 order by razonsocial";
 		const arraydata = await get_data_dynamic(query);
-		cargarselect2("#proveedorx", arraydata, "codigoclienten", "name")
+		cargarselect2("#proveedorx", arraydata, "codigoclienten", "name", ["ruc"])
 
 	}
 </script>
