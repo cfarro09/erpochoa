cc7bed1a7ce852deb85aa1855a287c3e6266ea3e
Carlos Farro <fdcarlosd1@gmail.com>
2020-01-28 23:05:42 -0500

    avac

--- get_data_dynamic2.php
+++ get_data_dynamic2.php
@@ -1,16 +1,26 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
+
 require_once('Connections/Ventas.php');
 mysql_select_db($database_Ventas, $Ventas);
+
 
 //  table
 $query = $_POST['query'];
 
+mysql_query("SET NAMES 'utf8'", $Ventas);
+
 $query_res = mysql_query($query, $Ventas) or die(mysql_error());
 $res = array();
+try{
+    while($fila = mysql_fetch_object($query_res)) {
+        
+        array_push($res, $fila);
+    }
+    die(json_encode($res, 128));
 
-while($fila = mysql_fetch_object($query_res)) {
-    array_push($res, $fila);
+}catch(Exception $e){
+    var_dump($e->getMessage());
 }
 
-die(json_encode($res, 128));
 
