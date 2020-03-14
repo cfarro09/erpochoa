2fd8823135c076d1db2820bb283c4703d1c06b65
Carlos Farro <fdcarlosd1@gmail.com>
2020-03-13 23:16:25 -0500

    avnce

--- includes/common/KT_functions.inc.php
+++ includes/common/KT_functions.inc.php
@@ -459,15 +459,9 @@ function KT_redir($url) {
 		$url = str_replace(" ","%20",$url);
 		if (KT_is_ajax_request()) {
 			header("Kt_location: ".$url);
-			// echo "Redirecting to: " . $url;
+			echo "Redirecting to: " . $url;
 		} else {
-
-			ob_start();
-			while (ob_get_status()) 
-			{
-				ob_end_clean();
-			}
-			header("Location: $url");
+			header("Location: ". $url);
 		}
 	}
 	exit;
